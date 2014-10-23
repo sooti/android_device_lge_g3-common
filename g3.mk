46095141c571384565054a74b9f06cfd4a9bce26
Dan Pasanen <dan.pasanen@gmail.com>
2014-10-23 08:36:34 -0500

    g3: add consumerir hal and necessary perms
    Change-Id: Ia5be9e0447a16ea3f4aeded4e9e20b8b8a154025

--- g3.mk
+++ g3.mk
@@ -27,6 +27,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
@@ -108,6 +109,10 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_PACKAGES += \
     gps.msm8974
+
+# IRRC (Infrared remote)
+PRODUCT_PACKAGES += \
+    consumerir.msm8974
 
 # IRSC
 PRODUCT_COPY_FILES += \
