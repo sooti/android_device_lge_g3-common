0970902b9a15ae1c651e52f180f8a91e29ccaf82
Dan Pasanen <dan.pasanen@gmail.com>
2014-10-24 11:12:10 -0500

    camera: build hal from caf
    * AU_LINUX_ANDROID_LNX.LA.3.5.1.04.04.02.032.082
    
    Change-Id: I25943db8b7d0b024285c8628bd66b5107240c069

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,15 +1,14 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
+MM_V4L2_DRIVER_LIST += msm8960
+MM_V4L2_DRIVER_LIST += msm8974
+MM_V4L2_DRIVER_LIST += msm8226
+MM_V4L2_DRIVER_LIST += msm8610
+MM_V4L2_DRIVER_LIST += msm_bronze
+MM_V4L2_DRIVER_LIST += msm8916
 
-LOCAL_SRC_FILES := \
-    CameraWrapper.cpp
-
-LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_SHARED_LIBRARY)
-#include $(BUILD_HEAPTRACKED_SHARED_LIBRARY)
+ifeq ($(call is-board-platform-in-list,$(MM_V4L2_DRIVER_LIST)),true)
+  ifneq ($(USE_CAMERA_STUB),true)
+    ifneq ($(BUILD_TINY_ANDROID),true)
+      include $(call all-subdir-makefiles)
+    endif
+  endif
+endif
